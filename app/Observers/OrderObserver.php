fdbfeee889fbd8d9c50da84f3564ed4a8e38b557
Jaloliddin <jalol5060@gmail.com>
2025-09-29 21:06:58 +0500

    some changes

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -7,21 +7,22 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 
 class OrderObserver
 {
-    public function saving(Order $order)
+    public function updated(Order $order)
     {
         // dd($order);
-        $message = "🛒 Yangi buyurtma:\n";
-        $message .= "Buyurtma ID: {$order->id}\n";
-        $message .= "Mijoz: {$order->customer->first_name}\n";
-        $message .= "Narxi: {$order->total_price} so'm\n";
-        $message .= "Asl narxi: {$order->income_price} so'm\n";
+        $message = "🛒 *Yangi buyurtma:*\n";
+        $message .= "🆔 *Buyurtma ID:* {$order->id}\n";
+        $message .= "👤 *Mijoz:* {$order->customer->first_name}\n";
+        $message .= "💰 *Narxi:* {$order->total_price} so'm\n";
+        $message .= "🏷️ *Asl narxi:* {$order->income_price} so'm\n";
         $revenue = $order->total_price - $order->income_price;
-        $message .= "Foyda: {$revenue} so'm\n";
+        $message .= "📈 *Foyda:* {$revenue} so'm\n";
 
 
         Telegram::sendMessage([
             'chat_id' => env('TELEGRAM_CHAT_ID'),
             'text' => $message,
+            'parse_mode' => 'Markdown',
         ]);
     }
 }
