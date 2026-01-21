393cb04cace131652731146c813f10ff5354fae3
Jaloliddin <jalol5060@gmail.com>
2026-01-22 02:29:42 +0500

    bug fixes

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Order;
+use App\Models\Product;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class OrderObserver
@@ -10,14 +11,24 @@ class OrderObserver
     public function updated(Order $order)
     {
 
-        $total_price = number_format($order->total_price,0,',',' ');
-        $income_price = number_format($order->income_price,0,',',' ');
+        $total_price = number_format($order->total_price, 0, ',', ' ');
+        $income_price = number_format($order->income_price, 0, ',', ' ');
         $message = "🛒 *Yangi buyurtma:*\n";
         $message .= "🆔 *Buyurtma ID:* {$order->id}\n";
+        $message .= "---------------------------\n";
+        foreach ($order->items as $item) {
+
+            $product = Product::find($item->product_id);
+            // $product = Product::find($item->product_id);
+            // Har bir mahsulot nomi, miqdori va narxini qo'shamiz
+            $productName = $product->name; // Agar OrderItem'da product bog'lanishi bo'lsa
+            $message .= "🔹 {$productName} — {$item->quantity} dona x " . number_format($item->price) . " so'm\n";
+        }
+        $message .= "---------------------------\n";
         $message .= "👤 *Mijoz:* {$order->customer->first_name}\n";
         $message .= "💰 *Narxi:* {$total_price} so'm\n";
         $message .= "🏷️ *Asl narxi:* {$income_price} so'm\n";
-        $revenue = number_format($order->total_price - $order->income_price,0,',',' ');
+        $revenue = number_format($order->total_price - $order->income_price, 0, ',', ' ');
         $message .= "📈 *Foyda:* {$revenue} so'm\n";
 
 
